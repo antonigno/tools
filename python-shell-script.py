2a26be7ad3bf5753b6af7956bd4e010d0e83a0c1
antonigno <stefano.borgia@gmail.com>
2015-01-03 12:27:17 +0000

    added script to manipulate standard inputs

---
+++ python-shell-script.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import sys
+
+if __name__ == "__main__":
+    names = {}
+    for name in sys.stdin.readlines():
+            name = name.strip()
+            if name in names:
+                    names[name] += 1
+            else:
+                    names[name] = 1
+    for name, count in names.iteritems():
+        print "mv "+name.replace(" ", "\ ")+" Video/"
+
